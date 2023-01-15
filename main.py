509c02062d22d7e0b351633f20203f47745a1af2
Illiyyin Akbar Ariyanto <58885451+IlliyyinAkbar2002@users.noreply.github.com>
2023-01-15 17:07:37 +0700

    Update main 2.0
    Calculator IBM 2.0

--- main.py
+++ main.py
@@ -12,8 +12,18 @@ formula = second / first ** 2
 
 # convert float to integer
 # print(type(formula))
+# condition 
 result = int(formula)
-
-# Print calculate height and weight IBM formula
-print(result)
+if result <= 18:
+    print("Your BMI 18, is you are underweight.")
+elif result <= 24:
+    print("Your BMI is 23, you have a normal weight.")
+elif result <= 27:
+    print("Your BMI is 26, you are slightly overweight.")
+elif result <= 30:
+    print("Your BMI is 29, you are slightly overweight.")
+elif result >= 30:
+    print("Your BMI is 33, you are obese.")
+elif result >= 40:
+    print("Your BMI is 40, you are clinically obese.")
 
